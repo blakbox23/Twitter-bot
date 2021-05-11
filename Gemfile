91b6aafb5da650e1a13da6bf56ee9f65c1386f62
blakbox23 <boudii30@gmail.com>
2021-05-12 01:22:34 +0300

    Remove robocop errors

--- Gemfile
+++ Gemfile
@@ -7,5 +7,5 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
 gem 'dotenv'
 gem 'rufus-scheduler'
-gem 'twitter twitter'
+gem 'twitter'
 gem 'tzinfo'
--- bin/main.rb
+++ bin/main.rb
@@ -1,95 +1,147 @@
 #!/usr/bin/env ruby
+
+# rubocop:disable Metrics/PerceivedComplexity
+# rubocop:disable Metrics/AbcSize
+# rubocop:disable Metrics/CyclomaticComplexity
+
 require 'dotenv/load'
 require 'twitter'
 require 'rufus-scheduler'
-require_relative '../lib/talk.rb'
+require_relative '../lib/talk'
 
 scheduler = Rufus::Scheduler.new
-$client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV['consumer_key']
-  config.consumer_secret     = ENV['consumer_secret']
-  config.access_token        = ENV['access_token']
-  config.access_token_secret = ENV['access_token_secret']
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = 'consumer_key'
+  config.consumer_secret = 'consumer_secret'
+  config.access_token = 'access_token'
+  config.access_token_secret = 'access_token_secret'
+end
+
+# TWEETS
+class TweetCls
+  def initialize(client)
+    @client = client
+  end
+
+  def tweet_mthd
+    benefits = RubyBenefits.new
+    puts 'tweeted'
+    @client.update(benefits.benefits_of_ruby.sample.to_s)
+    benefits.benefits_of_ruby.sample.to_s
+  end
+end
+
+tweet_cls = TweetCls.new(client)
+
+scheduler.every '35' do
+  tweet_cls.tweet_mthd
 end
 
 # RETWEETS
 class RetweetCls
-    $content_repeat = []
-    def retweet_mthd
-      $client.search("Coding", result_type: "recent", lang: "en").take(1).each do |tweet|
-        username = (tweet.user.screen_name).upcase 
-        if !($content_repeat.include? tweet.id ) && (tweet.retweeted_status==false) && !(tweet.text.include? "http") then
-          $content_repeat.push(tweet.id)
-           $client.retweet(tweet)
-          return $client.retweet(tweet)
-        else
-          puts "bad retweet"   
-        end
+  def initialize(client)
+    @client = client
+    @content_repeat = []
+  end
+
+  def retweet_mthd
+    @client.search('Coding', result_type: 'recent', lang: 'en').take(1).each do |tweet|
+      username = tweet.user.screen_name.upcase
+      if !(@content_repeat.include? tweet.id) && (tweet.retweeted_status == false) && !(tweet.text.include? 'http')
+        puts "#{username} :  #{tweet.text} : USER-id: #{tweet.user.id}"
+        @content_repeat.push(tweet.id)
+        puts 'retweeted'
+
+        @client.retweet(tweet)
+        return @client.retweet(tweet)
+      else
+        puts 'bad retweet'
       end
     end
   end
-  retweet_cls = RetweetCls.new
-  
-  scheduler.every '2m' do
-    retweet_cls.retweet_mthd
+end
+retweet_cls = RetweetCls.new(client)
+
+scheduler.every '19m' do
+  retweet_cls.retweet_mthd
+end
+
+# REPLY
+class ReplyCls
+  def initialize(client)
+    @client = client
+    @already_replied = []
   end
 
-  # TWEETS
-class TweetCls
-    def tweet_mthd
-      benefits = RubyBenefits.new
-      puts "tweeted"
-       $client.update("#{benefits.benefits_of_ruby.sample}")
-      return "#{benefits.benefits_of_ruby.sample}"
-    end
-  end
-  
-  tweet_cls = TweetCls.new
-  
-  scheduler.every '5m' do
-      tweet_cls.tweet_mthd
-  end
-  
-  # REPLY
-class ReplyCls
-  $already_replied = []
-  
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/BlockLength
+
   def reply_mthd
     talker = Talker.new
-    $client.search( "@blakbot23", result_type: "recent").take(1).each do |tweet|
-  
-    if  !($already_replied.include? tweet.id ) then
-      tweet_text = tweet.text
-      $already_replied.push(tweet.id)
+    @client.search('@blakbot23', result_type: 'recent').take(1).each do |tweet|
+      # puts tweet.user.screen_name
 
-      if talker.salutations_mthd.any? {|item| tweet_text.downcase.split(" ").include?(item)} then
-        $client.update("@#{tweet.user.screen_name}  #{talker.multi_salutations_mthd.sample.capitalize} human, I'm a bot", in_reply_to_status_id: tweet.id)
-        return $client.update("@#{tweet.user.screen_name}  #{talker.multi_salutations_mthd.sample.capitalize} human, I'm a bot", in_reply_to_status_id: tweet.id)
-      elsif talker.multi_salutations_mthd.any? {|item| tweet_text.downcase.include?(item)} then
-        $client.update("@#{tweet.user.screen_name} #{talker.salutations_mthd.sample.capitalize}", in_reply_to_status_id: tweet.id)
-        return $client.update("@#{tweet.user.screen_name} #{talker.salutations_mthd.sample.capitalize}", in_reply_to_status_id: tweet.id)
-      elsif talker.purpose_quiz_mthd.any? {|item| tweet_text.downcase.include?(item)} then
-        $client.update("@#{tweet.user.screen_name} #{talker.purpose_ans_mthd.sample.capitalize}", in_reply_to_status_id: tweet.id)
-        return $client.update("@#{tweet.user.screen_name} #{talker.purpose_ans_mthd.sample.capitalize}", in_reply_to_status_id: tweet.id)
-      elsif talker.opinion_quiz_mthd.any? {|item| tweet_text.downcase.include? (item)} then
-        $client.update("@#{tweet.user.screen_name}  #{talker.opinions.sample.capitalize}", in_reply_to_status_id: tweet.id)
-        return $client.update("@#{tweet.user.screen_name}  #{talker.opinions.sample.capitalize}", in_reply_to_status_id: tweet.id)
-      elsif talker.byes_mthd.any? {|item| tweet_text.downcase.include? (item)} then
-        $client.update("@#{tweet.user.screen_name} #{talker.byes_mthd.sample.capitalize} see you later", in_reply_to_status_id: tweet.id)
-        return $client.update("@#{tweet.user.screen_name} #{talker.byes_mthd.sample.capitalize} see you later", in_reply_to_status_id: tweet.id)
-      else     
-        $client.update("@#{tweet.user.screen_name} Sorry I didn't get that", in_reply_to_status_id: tweet.id)
-        return $client.update("@#{tweet.user.screen_name} Sorry I didn't get that", in_reply_to_status_id: tweet.id)
+      next if @already_replied.include? tweet.id
+
+      tweet_text = tweet.text
+      puts "replied to: #{tweet.user.screen_name}"
+
+      puts tweet.retweeted_status.text
+      @already_replied.push(tweet.id)
+      if talker.salutations_mthd.any? { |item| tweet_text.downcase.split.include?(item) }
+        puts 'right : #replied to salutation'
+        @client.update(
+          "@#{tweet.user.screen_name}  #{talker.multi_salutations_mthd.sample.capitalize} human, I'm a bot",
+          in_reply_to_status_id: tweet.id
+        )
+        return @client.update(
+          "@#{tweet.user.screen_name}  #{talker.multi_salutations_mthd.sample.capitalize} human, I'm a bot",
+          in_reply_to_status_id: tweet.id
+        )
+      elsif talker.multi_salutations_mthd.any? { |item| tweet_text.downcase.include?(item) }
+        @client.update("@#{tweet.user.screen_name} #{talker.salutations_mthd.sample.capitalize}",
+                       in_reply_to_status_id: tweet.id)
+        return @client.update("@#{tweet.user.screen_name} #{talker.salutations_mthd.sample.capitalize}",
+                              in_reply_to_status_id: tweet.id)
+        # puts 'right : replied to multi_salutation'
+      elsif talker.purpose_quiz_mthd.any? { |item| tweet_text.downcase.include?(item) }
+        @client.update("@#{tweet.user.screen_name} #{talker.purpose_ans_mthd.sample.capitalize}",
+                       in_reply_to_status_id: tweet.id)
+        return @client.update("@#{tweet.user.screen_name} #{talker.purpose_ans_mthd.sample.capitalize}",
+                              in_reply_to_status_id: tweet.id)
+        # puts 'right : replied to purpose'
+      elsif talker.opinion_quiz_mthd.any? { |item| tweet_text.downcase.include?(item) }
+        puts 'right : replied to opinion'
+        @client.update("@#{tweet.user.screen_name}  #{talker.opinions.sample.capitalize}",
+                       in_reply_to_status_id: tweet.id)
+        return @client.update("@#{tweet.user.screen_name}  #{talker.opinions.sample.capitalize}",
+                              in_reply_to_status_id: tweet.id)
+      elsif talker.byes_mthd.any? { |item| tweet_text.downcase.include?(item) }
+        puts 'right: replied to thanks/byes'
+        @client.update("@#{tweet.user.screen_name} #{talker.byes_mthd.sample.capitalize} see you later",
+                       in_reply_to_status_id: tweet.id)
+        return @client.update("@#{tweet.user.screen_name} #{talker.byes_mthd.sample.capitalize} see you later",
+                              in_reply_to_status_id: tweet.id)
+      else
+        @client.update("@#{tweet.user.screen_name} Sorry I didn't get that", in_reply_to_status_id: tweet.id)
+        puts 'stopped'
+        return @client.update("@#{tweet.user.screen_name} Sorry I didn't get that",
+                              in_reply_to_status_id: tweet.id)
       end
     end
   end
- end
 end
-  
-  reply_cls = ReplyCls.new
-  
-  scheduler.every '31s' do
-    reply_cls.reply_mthd
-  end
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/BlockLength
 
-  scheduler.join
+reply_cls = ReplyCls.new(client)
+
+scheduler.every '20' do
+  reply_cls.reply_mthd
+end
+
+scheduler.join
+
+# rubocop:enable Metrics/PerceivedComplexity
+# rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/CyclomaticComplexity
