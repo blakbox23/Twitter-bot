4908ef0524ccebd13c3baba92c30220579ae9229
blakbox23 <boudii30@gmail.com>
2021-05-11 17:00:27 +0300

    Add Tweet functionality

--- bin/main.rb
+++ bin/main.rb
@@ -19,9 +19,7 @@ class RetweetCls
       $client.search("Coding", result_type: "recent", lang: "en").take(1).each do |tweet|
         username = (tweet.user.screen_name).upcase 
         if !($content_repeat.include? tweet.id ) && (tweet.retweeted_status==false) && !(tweet.text.include? "http") then
-          puts "#{username} :  #{tweet.text} : USER-id: #{tweet.user.id}"
           $content_repeat.push(tweet.id)
-          puts "retweeted"
            $client.retweet(tweet)
           return $client.retweet(tweet)
         else
@@ -35,3 +33,20 @@ class RetweetCls
   scheduler.every '2m' do
     retweet_cls.retweet_mthd
   end
+
+  # TWEETS
+class TweetCls
+    def tweet_mthd
+      benefits = RubyBenefits.new
+      puts "tweeted"
+       $client.update("#{benefits.benefits_of_ruby.sample}")
+      return "#{benefits.benefits_of_ruby.sample}"
+    end
+  end
+  
+  tweet_cls = TweetCls.new
+  
+  scheduler.every '5m' do
+      tweet_cls.tweet_mthd
+  end
+  
