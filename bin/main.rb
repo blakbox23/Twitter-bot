0420bb84c23e04b6ac1c74ce784fdec48e475ec9
blakbox23 <boudii30@gmail.com>
2021-05-11 16:58:52 +0300

    Add Retweet functionality

--- bin/main.rb
+++ bin/main.rb
@@ -2,7 +2,7 @@
 require 'dotenv/load'
 require 'twitter'
 require 'rufus-scheduler'
-require_relative './talk.rb'
+require_relative '../lib/talk.rb'
 
 scheduler = Rufus::Scheduler.new
 $client = Twitter::REST::Client.new do |config|
@@ -11,3 +11,27 @@ $client = Twitter::REST::Client.new do |config|
   config.access_token        = ENV['access_token']
   config.access_token_secret = ENV['access_token_secret']
 end
+
+# RETWEETS
+class RetweetCls
+    $content_repeat = []
+    def retweet_mthd
+      $client.search("Coding", result_type: "recent", lang: "en").take(1).each do |tweet|
+        username = (tweet.user.screen_name).upcase 
+        if !($content_repeat.include? tweet.id ) && (tweet.retweeted_status==false) && !(tweet.text.include? "http") then
+          puts "#{username} :  #{tweet.text} : USER-id: #{tweet.user.id}"
+          $content_repeat.push(tweet.id)
+          puts "retweeted"
+           $client.retweet(tweet)
+          return $client.retweet(tweet)
+        else
+          puts "bad retweet"   
+        end
+      end
+    end
+  end
+  retweet_cls = RetweetCls.new
+  
+  scheduler.every '2m' do
+    retweet_cls.retweet_mthd
+  end
