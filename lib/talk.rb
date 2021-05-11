342f84561e685bace41ef3becfbbf6f84bcfd96a
blakbox23 <boudii30@gmail.com>
2021-05-11 17:05:22 +0300

    Add interaction classes in talk.rb

---
+++ lib/talk.rb
@@ -0,0 +1,55 @@
+class Talker
+
+    def opinion_quiz_mthd
+      opinion_quiz = ["about ruby"]
+    end
+  
+    def opinions
+      opinion = [
+      "Everything in Ruby is an expression even statements",
+      "has an option of code embedding ie interpolation",
+      "has four level of variables ie global, class, instance & local",
+      "is highly object oriented programming language with mixins & metaclasses",
+      "has a use of dynamic typing & duck typing"]
+    end
+  
+    def byes_mthd
+      byes = ["thanks", "thank you", "bye"]
+    end
+  
+    def salutations_mthd
+      salutations = ["hey", "hello", "hi", "howdy"]
+    end
+  
+    def multi_salutations_mthd
+      multi_salutations = ["how are you", "how is it going", "good morning"]
+    end
+  
+    def purpose_quiz_mthd
+      purpose_quiz = [ "what do you do", "who made you", "what can you do"]
+    end
+    
+    def purpose_ans_mthd
+      purpose_ans = ["I'm learning Ruby", "I say random fact about Ruby", "Ruby"]
+    end
+  end
+  
+  class RubyBenefits
+      def benefits_of_ruby 
+        arr = [
+        "Everything in Ruby is an expression even statements",
+        "Ruby has an option of code embedding ie interpolation",
+        "Ruby has four level of variables ie global, class, instance & local",
+        "Ruby is highly object oriented programming language with mixins & metaclasses",
+        "Ruby has a use of dynamic typing & duck typing"
+      ]
+    end
+  end
+  
+  class BlackList
+      def blacklist_mthd
+          blacklist_arr = ["drag", "fuck", "nigga"]
+      end
+  end
+  
+  
